e7bd3d29d648ea0bf905d743fad3731acd9b119e
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-13 09:39:53 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install gcc g++ make
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs -y
-sudo npm install -g forever
-# sudo npm i dotenv
-# sudo npm audit fix --force
 
+# Install forever module 
+# https://www.npmjs.com/package/forever
+sudo npm install pm2 -g
 
 
 
