e1718256879f44c2e2d9df4c474dade52e517aa0
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-14 13:07:49 -0400

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,8 @@
 # Stop all servers and start the server as a daemon 
 cd /home/ubuntu/myportfolio
 pm2 stop all
+rm -rf node_modules
+sudo npm install
 pm2 start npm -- start
 # forever stopall
 # sudo forever --sourceDir /home/ubuntu/myportfolio -c "npm start"  --minUptime 10000 --spinSleepTime 1000
