ba915c2271a9e7bc6033b4060aa553edfd0665dd
sarhlarm <98288904+sarhlarm@users.noreply.github.com>
2022-03-17 14:19:42 -0400

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,8 +18,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                THIS IS
+                <strong className="main-name"> SALAM PROJECT FROM CLOUD REALITY</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
